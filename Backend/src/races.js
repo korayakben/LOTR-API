c7a09ff6594ba8318b1461df56ff924f5610b458
Koray <korayakben@gmail.com>
2026-03-06 16:56:43 +0300

    Change import syntax to use 'with' for JSON

--- Backend/src/races.js
+++ Backend/src/races.js
@@ -1,15 +1,15 @@
-import dark_beings from "./datas/dark-beings.json" assert {type: "json"};
-import dragons from "./datas/dragons.json" assert {type: "json"};
-import dwarves from "./datas/dwarves.json" assert {type: "json"};
-import elves from "./datas/elves.json" assert {type: "json"};
-import ents from "./datas/ents.json" assert {type: "json"};
-import hobbits from "./datas/hobbits.json" assert {type: "json"};
-import humans from "./datas/humans.json" assert {type: "json"};
-import locations from "./datas/locations.json" assert {type: "json"};
-import orcs_urukhais from "./datas/orcs-urukhais.json" assert {type: "json"};
-import rings from "./datas/rings.json" assert {type: "json"};
-import trolls from "./datas/trolls.json" assert {type: "json"};
-import wizards from "./datas/wizards.json" assert {type: "json"};
+import dark_beings from "./datas/dark-beings.json" with {type: "json"};
+import dragons from "./datas/dragons.json" with {type: "json"};
+import dwarves from "./datas/dwarves.json" with {type: "json"};
+import elves from "./datas/elves.json" with {type: "json"};
+import ents from "./datas/ents.json" with {type: "json"};
+import hobbits from "./datas/hobbits.json" with {type: "json"};
+import humans from "./datas/humans.json" with {type: "json"};
+import locations from "./datas/locations.json" with {type: "json"};
+import orcs_urukhais from "./datas/orcs-urukhais.json" with {type: "json"};
+import rings from "./datas/rings.json" with {type: "json"};
+import trolls from "./datas/trolls.json" with {type: "json"};
+import wizards from "./datas/wizards.json" with {type: "json"};
 
 export const race_dict = {
     "dark_beings": dark_beings,
@@ -24,4 +24,5 @@ export const race_dict = {
     "rings": rings,
     "trolls": trolls,
     "wizards": wizards
+
 };
